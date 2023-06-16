5ecbbb434992d535696efa5e4df5d01ca2a0563c
UIMSolutions <ui.manufaktur@gmail.com>
2023-06-16 02:28:28 +0200

    Change namesapece

---
+++ source/apps/systems/views/components/forms/groups/otp.d
@@ -0,0 +1,20 @@
+module apps.systems.views.components.forms.groups.otp;
+
+@safe:
+import apps.systems;
+
+/* class DCTLOtpFormInput : DSwitchFormInput {
+  mixin(ViewComponentThis!("CTLOtpFormInput"));
+
+  override void initialize(Json configSettings = Json(null)) {
+    super.initialize(configSettings);
+
+    this
+    .id("entity_otp")
+    .name("entity_otp")
+    .label("Einmaliges Passwort")
+    .fieldName("otp")
+    .fieldValue("false");
+  }
+}
+mixin(ViewComponentCalls!("CTLOtpFormInput")); */
---
+++ source/apps/systems/views/components/forms/groups/package.d
@@ -0,0 +1,15 @@
+module apps.systems.views.components.forms.groups;
+
+@safe:
+import apps.systems;
+
+public import apps.systems.views.components.forms.groups.handler;
+
+public import apps.systems.views.components.forms.groups.active;
+public import apps.systems.views.components.forms.groups.otp;
+public import apps.systems.views.components.forms.groups.password;
+public import apps.systems.views.components.forms.groups.valid;
+public import apps.systems.views.components.forms.groups.validfrom;
+public import apps.systems.views.components.forms.groups.validuntil;
+
+
